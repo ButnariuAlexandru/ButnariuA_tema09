1b4e15ffbc74684b87dc7021229a2f0afc1da9ea
ButnariuAlexandru <alexandru.butnariu1@student.usv.ro>
2025-12-02 19:24:56 +0200

    Add files via upload

---
+++ SlotMachineDemo/Slots.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace SlotMachineDemo
+{
+    partial class Slots
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.BPull = new System.Windows.Forms.Button();
+            this.UDCycles = new System.Windows.Forms.NumericUpDown();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.TbMesaj = new System.Windows.Forms.TextBox();
+            this.TbCredits = new System.Windows.Forms.TextBox();
+            this.Credite = new System.Windows.Forms.Label();
+            this.title = new System.Windows.Forms.Label();
+            this.pb7 = new System.Windows.Forms.PictureBox();
+            this.pb8 = new System.Windows.Forms.PictureBox();
+            this.pb9 = new System.Windows.Forms.PictureBox();
+            this.pb4 = new System.Windows.Forms.PictureBox();
+            this.pb5 = new System.Windows.Forms.PictureBox();
+            this.pb6 = new System.Windows.Forms.PictureBox();
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            this.pb2 = new System.Windows.Forms.PictureBox();
+            this.pb3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.UDCycles)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BPull
+            // 
+            this.BPull.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.BPull.Font = new System.Drawing.Font("Microsoft Uighur", 20.29091F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BPull.ForeColor = System.Drawing.Color.Maroon;
+            this.BPull.Location = new System.Drawing.Point(705, 524);
+            this.BPull.Name = "BPull";
+            this.BPull.Size = new System.Drawing.Size(129, 72);
+            this.BPull.TabIndex = 4;
+            this.BPull.Text = "TRAGE!!!";
+            this.BPull.UseVisualStyleBackColor = false;
+            // 
+            // UDCycles
+            // 
+            this.UDCycles.Location = new System.Drawing.Point(705, 480);
+            this.UDCycles.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.UDCycles.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.UDCycles.Name = "UDCycles";
+            this.UDCycles.Size = new System.Drawing.Size(129, 20);
+            this.UDCycles.TabIndex = 5;
+            this.UDCycles.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 80;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 80;
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 80;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.groupBox1.Controls.Add(this.pb7);
+            this.groupBox1.Controls.Add(this.pb8);
+            this.groupBox1.Controls.Add(this.pb9);
+            this.groupBox1.Controls.Add(this.pb4);
+            this.groupBox1.Controls.Add(this.pb5);
+            this.groupBox1.Controls.Add(this.pb6);
+            this.groupBox1.Controls.Add(this.pb1);
+            this.groupBox1.Controls.Add(this.pb2);
+            this.groupBox1.Controls.Add(this.pb3);
+            this.groupBox1.Location = new System.Drawing.Point(74, 58);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(760, 416);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            // 
+            // TbMesaj
+            // 
+            this.TbMesaj.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.TbMesaj.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TbMesaj.Font = new System.Drawing.Font("Monotype Corsiva", 20.29091F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.TbMesaj.Location = new System.Drawing.Point(244, 500);
+            this.TbMesaj.Name = "TbMesaj";
+            this.TbMesaj.ReadOnly = true;
+            this.TbMesaj.Size = new System.Drawing.Size(433, 35);
+            this.TbMesaj.TabIndex = 10;
+            // 
+            // TbCredits
+            // 
+            this.TbCredits.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.70909F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.TbCredits.Location = new System.Drawing.Point(95, 523);
+            this.TbCredits.Name = "TbCredits";
+            this.TbCredits.ReadOnly = true;
+            this.TbCredits.Size = new System.Drawing.Size(122, 35);
+            this.TbCredits.TabIndex = 9;
+            // 
+            // Credite
+            // 
+            this.Credite.AutoSize = true;
+            this.Credite.Font = new System.Drawing.Font("Microsoft YaHei", 11.78182F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Credite.Location = new System.Drawing.Point(101, 495);
+            this.Credite.Name = "Credite";
+            this.Credite.Size = new System.Drawing.Size(81, 25);
+            this.Credite.TabIndex = 11;
+            this.Credite.Text = "Credite:";
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Papyrus", 22.25455F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.ForeColor = System.Drawing.Color.Green;
+            this.title.Location = new System.Drawing.Point(283, 1);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(334, 54);
+            this.title.TabIndex = 12;
+            this.title.Text = "Winter Wonderland";
+            // 
+            // pb7
+            // 
+            this.pb7.Location = new System.Drawing.Point(32, 277);
+            this.pb7.Name = "pb7";
+            this.pb7.Size = new System.Drawing.Size(171, 115);
+            this.pb7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb7.TabIndex = 13;
+            this.pb7.TabStop = false;
+            // 
+            // pb8
+            // 
+            this.pb8.Location = new System.Drawing.Point(296, 277);
+            this.pb8.Name = "pb8";
+            this.pb8.Size = new System.Drawing.Size(171, 115);
+            this.pb8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb8.TabIndex = 12;
+            this.pb8.TabStop = false;
+            // 
+            // pb9
+            // 
+            this.pb9.Location = new System.Drawing.Point(560, 277);
+            this.pb9.Name = "pb9";
+            this.pb9.Size = new System.Drawing.Size(171, 115);
+            this.pb9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb9.TabIndex = 11;
+            this.pb9.TabStop = false;
+            // 
+            // pb4
+            // 
+            this.pb4.Location = new System.Drawing.Point(32, 10);
+            this.pb4.Name = "pb4";
+            this.pb4.Size = new System.Drawing.Size(171, 115);
+            this.pb4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb4.TabIndex = 10;
+            this.pb4.TabStop = false;
+            // 
+            // pb5
+            // 
+            this.pb5.Location = new System.Drawing.Point(296, 10);
+            this.pb5.Name = "pb5";
+            this.pb5.Size = new System.Drawing.Size(171, 115);
+            this.pb5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb5.TabIndex = 9;
+            this.pb5.TabStop = false;
+            // 
+            // pb6
+            // 
+            this.pb6.Location = new System.Drawing.Point(560, 10);
+            this.pb6.Name = "pb6";
+            this.pb6.Size = new System.Drawing.Size(171, 115);
+            this.pb6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb6.TabIndex = 8;
+            this.pb6.TabStop = false;
+            // 
+            // pb1
+            // 
+            this.pb1.Location = new System.Drawing.Point(32, 142);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(171, 115);
+            this.pb1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb1.TabIndex = 7;
+            this.pb1.TabStop = false;
+            // 
+            // pb2
+            // 
+            this.pb2.Location = new System.Drawing.Point(296, 142);
+            this.pb2.Name = "pb2";
+            this.pb2.Size = new System.Drawing.Size(171, 115);
+            this.pb2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb2.TabIndex = 6;
+            this.pb2.TabStop = false;
+            // 
+            // pb3
+            // 
+            this.pb3.Location = new System.Drawing.Point(560, 142);
+            this.pb3.Name = "pb3";
+            this.pb3.Size = new System.Drawing.Size(171, 115);
+            this.pb3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb3.TabIndex = 2;
+            this.pb3.TabStop = false;
+            // 
+            // Slots
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Ivory;
+            this.ClientSize = new System.Drawing.Size(911, 608);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.Credite);
+            this.Controls.Add(this.TbMesaj);
+            this.Controls.Add(this.TbCredits);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.UDCycles);
+            this.Controls.Add(this.BPull);
+            this.Name = "Slots";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.UDCycles)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pb7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pb3;
+        private System.Windows.Forms.Button BPull;
+        private System.Windows.Forms.NumericUpDown UDCycles;
+        private System.Windows.Forms.PictureBox pb2;
+        private System.Windows.Forms.PictureBox pb1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox TbMesaj;
+        private System.Windows.Forms.TextBox TbCredits;
+        private System.Windows.Forms.Label Credite;
+        private System.Windows.Forms.PictureBox pb7;
+        private System.Windows.Forms.PictureBox pb8;
+        private System.Windows.Forms.PictureBox pb9;
+        private System.Windows.Forms.PictureBox pb4;
+        private System.Windows.Forms.PictureBox pb5;
+        private System.Windows.Forms.PictureBox pb6;
+        private System.Windows.Forms.Label title;
+    }
+}
+
---
+++ SlotMachineDemo/Slots.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SlotMachineDemo
+{
+        public partial class Slots : Form
+        {
+            private List<System.Drawing.Image> imagini;
+            private Random rnd = new Random();
+            private Timer timer;
+            private int cicluriRamase;
+            private int credits = 500;
+
+
+        public Slots()
+            {
+                InitializeComponent();
+
+
+            TbCredits.Text = credits.ToString();
+            imagini = new List<System.Drawing.Image>()
+            {
+                Properties.Resources.img1,
+                Properties.Resources.img2,
+                Properties.Resources.img3,
+                Properties.Resources.img4,
+                Properties.Resources.img5,
+                Properties.Resources.img6
+            };
+                timer = new Timer();
+                timer.Interval = 500;
+                timer.Tick += Timer_Tick;
+
+                BPull.Click += BPull_Click;
+            }
+
+            private void BPull_Click(object sender, EventArgs e)
+            {
+            if (credits < 50)
+            {
+                MessageBox.Show("Nu mai ai suficient credite pentru a trage!",
+                                "Credit insuficient",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+                return; 
+            }
+            credits -= 50;
+            TbCredits.Text = credits.ToString();
+
+            cicluriRamase = (int)UDCycles.Value;
+                BPull.Enabled = false;
+
+                timer.Start();
+            }
+
+            private void Timer_Tick(object sender, EventArgs e)
+            {
+                // rulează până se termină ciclurile
+                if (cicluriRamase > 0)
+                {
+                    cicluriRamase--;
+                    RuleazaSloturi();
+                }
+                else
+                {
+                    timer.Stop();
+                    BPull.Enabled = true;
+                    VerificaRezultat();
+                }
+            }
+
+            private void RuleazaSloturi()
+            {
+                pb1.Image = imagini[rnd.Next(imagini.Count)];
+                pb2.Image = imagini[rnd.Next(imagini.Count)];
+                pb3.Image = imagini[rnd.Next(imagini.Count)];
+                pb4.Image = imagini[rnd.Next(imagini.Count)];
+                pb5.Image = imagini[rnd.Next(imagini.Count)];
+                pb6.Image = imagini[rnd.Next(imagini.Count)];
+                pb7.Image = imagini[rnd.Next(imagini.Count)];
+                pb8.Image = imagini[rnd.Next(imagini.Count)];
+                pb9.Image = imagini[rnd.Next(imagini.Count)];
+        }
+
+            private void VerificaRezultat()
+            {
+            if (pb1.Image == pb2.Image && pb2.Image == pb3.Image)
+            {
+                TbMesaj.Text = "🎉 Ai câștigat! Toate imaginile sunt identice!";
+                credits += 75;
+            }
+            else
+            {
+                TbMesaj.Text = "Ai pierdut! Poți încerca din nou!";
+            }
+
+            TbCredits.Text = credits.ToString();
+
+            // Permite o nouă tragere
+            BPull.Enabled = true;
+        }
+        }
+    }
+
+
